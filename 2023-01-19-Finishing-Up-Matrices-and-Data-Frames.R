6d02435ea61c3a46d175d2ae5392e33aca95330a
Abigail Griffin <abigailgriffin@Abigails-MacBook-Pro.local>
2023-01-19 15:49:13 -0500

    add assignment 4, 5, notes, start weekly

--- 2023-01-19-Finishing-Up-Matrices-and-Data-Frames.R
+++ 2023-01-19-Finishing-Up-Matrices-and-Data-Frames.R
@@ -54,4 +54,92 @@ orderedIris<-iris[order(iris$Petal.Length),] # $ finds the named element which i
 ## data frame$column # all data from column is extracted
 
 
+###### FUNCTIONS IN R!!
 
+# everything in R is a function
+sum(3,2) # sum
+3+2 # + sign is a function
+sd
+
+## User-defined functions
+functionName<-function(argX=defaultX,argY=defaultY){
+  # curly bracket starts the body of the function
+  # lines of R code and notes
+  # create local variables (only `visble` to R within the function)- if you say x<-1 in here, you can't pull x outside of the function
+  # return (z) - makes the variable part of the global envt
+  # whatever you specify up there^, you use down here in the function e.g
+  # argX<-c("IDK")
+}
+## e.g.
+myFunc<-function(a=3,b=4){
+  z<-a+b
+  return(z)
+}
+myFunc() # runs defaults
+myFunc(a=100,b=3.4)
+
+myFuncBad<-function(a=3, b=3)# SHOULD ALWAYS BE DEFINED WITHIN FUNCTION){
+z<-a+b
+  return(z)
+}
+z
+myFuncBad() # runs defaults but returns an error bc b not specified
+
+## MULTIPLE RETURN STATEMENTS
+############################################################
+# a lot of # are best way to annotate, can hide this line
+# FUNCTION: HardyWeinberg
+# input: an allele frequency p(0,1)
+# output: p and the frequencies of 3 genotypes AA AB BB
+#-----------------------------------------------------------
+HardyWeinberg<-function(p=runif(1)){
+  if (p > 1.0 | p < 0.0) {
+    return("Function failure: p must be >= 0.0 and <= 1.0")
+  }
+q<-1-p
+fAA<- p^2 # frequency of AA
+fAB<- 2*p*q
+fBB<- q^2
+vecOut<- signif(c(p=p,AA=fAA, AB=fAB, BB=fBB), digits=3)
+return(vecOut)
+} #there are 2{} because we're using the if statement. they need the condition first then return "Function failure..."
+HardyWeinberg()
+freqs<-HardyWeinberg()
+freqs
+HardyWeinberg(p=0.25)
+#### confused???
+
+#### CREATE A COMPLEX DEFAULT VALUE
+###########################################################
+# FUNCTION: fitLinear2
+## fits simple linear regression line
+## input: numeric list of predictor (x) and response (y)
+## outputs: slope and p-value
+
+fitLinear2<-function(p=NULL){
+    if(is.null(p)) {
+    p<-list(x=runif(20), y=runif(20)) # says we will make a variable p if we don't have one
+  }
+    myMod<-lm(p$x~p$y)
+    myOut<-c(slope=summary(myMod)$coefficients[2,1],
+           pValue=summary(myMod)$coefficients[2,4]) #that's the pvalue instead of the slope
+           plot(x=p$x, y=p$y) # quick plot to check output
+           return(myOut)
+}
+## from website
+fitLinear2 <- function(p=NULL){ #setting default to nothing, if p is null it will run and simulate if data were there - then when we put data in there it will work
+  if(is.null(p)) {
+    p <- list(x=runif(20),y=runif(20))
+  }
+  myMod <- lm(p$x~p$y) # fit the model
+  myOut <- c(slope=summary(myMod)$coefficients[2,1],
+             pValue=summary(myMod)$coefficients[2,4])
+  plot(x=p$x,y=p$y) # quick and dirty plot to check output
+  return(myOut)
+}
+
+# the if statement allows you to input something as a list function or will create something for you to put
+############################################################
+fitLinear2() #simulates p for us when p=NULL
+myPars<-list(x=1:10, y=runif(10))
+fitlinear2(p=myPars)
